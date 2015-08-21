69b21d1464ed104423b5bd793f6dbe8a3d06987e
Jeff Bruce <jeffbruce@gmail.com>
2015-08-21 09:24:27 -0400

    Initial commit.  Basic website design.

---
+++ deployment_notes.R
@@ -0,0 +1,3 @@
+# No sense doing this at the outset of the project.  
+# Makes sense to add it in right when you're about the deploy it, otherwise you're wasting time updating things.
+packrat::init('~/Projects/ShinyGenerateBrainReport')
